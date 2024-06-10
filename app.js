4418a10429785722b16bf12bfd3c698cc62dae4d
jayak <kumarijaya127@gmail.com>
2024-06-10 22:54:33 +0530

    added hash password

--- app.js
+++ app.js
@@ -41,24 +41,47 @@ bcrypt.genSalt(10, (err, salt) => {
 app.post("/create", upload.single("image"), (req, res) => {
   let { username, email, mobile, password, address } = req.body;
   bcrypt.genSalt(10, async (err, salt) => {
-    const image = req.file ? `/images/uploads/${req.file.filename}` : null;
-    const allUser = await userModel.create({
-      username,
-      email,
-      mobile,
-      password: hash,
-      address,
-      image,
-    });
-    bcrypt.hash("password", salt, (err, hash) => {
-      console.log("hash", hash);
+    if (err) {
+      console.error("Error generating salt:", err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    // Hash the password with the generated salt
+    bcrypt.hash(password, salt, async (err, hash) => {
+      if (err) {
+        console.error("Error hashing password:", err);
+        return res.status(500).send("Internal Server Error");
+      }
+
+      try {
+        // Create new user with hashed password
+        const image = req.file ? `/images/uploads/${req.file.filename}` : null;
+        const allUser = await userModel.create({
+          username,
+          email,
+          mobile,
+          password: hash, // Store hashed password
+          address,
+          image,
+        });
+
+        // Send response or perform further actions
+        //   res.send(allUser);
+        // console.log(req.file);
+        res.redirect("/userDataPage");
+        // res.send("User created successfully");
+      } catch (error) {
+        console.error("Error creating user:", error);
+        res.status(500).send("Internal Server Error");
+      }
     });
   });
-
-  //   res.send(allUser);
-  console.log(req.file);
-  res.redirect("/userDataPage");
 });
+
+//   res.send(allUser);
+//   console.log(req.file);
+//   res.redirect("/userDataPage");
+// });
 
 app.get("/userDataPage", async (req, res) => {
   let readUser = await userModel.find();
